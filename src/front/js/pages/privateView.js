b5e51959ca0370bd1a00b0800c54d4d43161daa6
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-09-18 14:35:31 +0000

    ejercicio terminado, arreglado page privateView

--- src/front/js/pages/privateView.js
+++ src/front/js/pages/privateView.js
@@ -1,4 +1,4 @@
-import react, { useEffect } from "react";
+import React, { useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 
 export const PrivateView = () => {
@@ -12,9 +12,16 @@ useEffect(() => {
     }
 }, [navigate]);
 
+const handleLogOut = () => {
+    sessionStorage.removeItem('token');
+
+    navigate('/LogIn');
+};
+
 return (
     <div>
         <h2>Private View!</h2>
+        <button onClick={handleLogOut} className="btn btn-danger"> Log Out </button> 
     </div>
 );
 };
